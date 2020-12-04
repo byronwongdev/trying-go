7cbbaf2b5c890a9d698613ae2c4f8b933c7f5c3f
byronwongdev <byronwongdev@gmail.com>
2020-12-05 01:05:48 +0800

    update

--- webapp/webapp.go
+++ webapp/webapp.go
@@ -6,7 +6,15 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Whoa, Go!")
+	fmt.Fprint(w, `<h1>Whoa, Go!</h1>
+	<p>Whoa, Go!</p>
+	<p>Whoa, Go!</p>
+	`)
+	fmt.Fprintf(w, "<h1>Whoa, Go!</h1>")
+	fmt.Fprintf(w, "<p>Whoa, Go!</p>")
+	fmt.Fprintf(w, "<p>Whoa, Go!</p>")
+	fmt.Fprintf(w, "<p>You %s even add %s</p>", "can", "<strong>variables</strong>")
+
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
